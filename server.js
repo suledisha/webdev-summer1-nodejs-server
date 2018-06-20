913b56cdd5fd684b871e5221b6fcb7c68dde2fae
Disha <suledisha1@ccs.neu.edu>
2018-06-20 17:56:49 -0400

    Adding CORS

--- server.js
+++ server.js
@@ -10,8 +10,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin",
-        "https://sule-disha-angular-app.herokuapp.com");
+    //res.header("Access-Control-Allow-Origin",
+        //"https://sule-disha-angular-app.herokuapp.com");
+
+    var allowedOrigins = ['http://localhost:4200','https://sule-disha-angular-app.herokuapp.com'];
+    var origin = req.headers.origin;
+    if(allowedOrigins.indexOf(origin) > -1){
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
