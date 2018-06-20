2892f6b0d92c3185429ed32c8b083d4a6bdb147f
Disha <suledisha1@ccs.neu.edu>
2018-06-20 17:00:41 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin",
-        "*");
+        "https://sule-disha-angular-app.herokuapp.com");
     res.header("Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods",
